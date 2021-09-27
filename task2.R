692765912c0140473577e9e38f942882c07e7fc7
MihailTchekmarev <mihail.tchekmarev@gmail.com>
2021-09-27 19:54:50 +0300

    Create task2.R

---
+++ task2.R
@@ -0,0 +1,26 @@
+func <- function(d) {
+  res <- d[[1]]
+  i <- 2
+  out <- c()
+  res$mean_temp <- res$temp
+  repeat {
+    for (elem in res$id) {
+      if (!(elem %in% data[[i]]$id)) {
+        out <- c(out, elem)
+      } else {
+        res$mean_temp[[which(res$id == elem)]] <- res$mean_temp[[which(res$id == elem)]] + data[[i]]$temp[[which(data[[i]]$id == elem)]]
+      }
+    }
+    
+    i <- i + 1
+    if (i > 7) {
+      res <- res[!(res$id %in% out),]
+      res$mean_temp <- res$mean_temp / 7
+      res1 <- data.frame(id = res$id, mean_temp = res$mean_temp)
+      return(res1)
+    }
+  }
+}
+
+#load("data.RData")
+#print(func(data))
